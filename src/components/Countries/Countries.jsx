63ebe0f5ebe02c441fdc0877f0857778a63b9d98
tanvi183 <mneshat7@gmail.com>
2025-09-18 21:51:25 -0700

    First Commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Show Country Component</h1>
+        </div>
+    );
+};
+
+export default Countries;
